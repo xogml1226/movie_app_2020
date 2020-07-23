a77ac64a82dfc49f4900420e86083ce0a981f32f
xogml1226 <xogml1226@naver.com>
2020-07-23 22:09:19 +0900

    Updates

---
+++ precache-manifest.e05d1f742ec80ef83406e30d72b7f2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3983efa1e3a7f8d47cbd590594c6479",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "bb26dc17e0d3df93c063",
+    "url": "/movie_app_2020/static/css/main.456e0784.chunk.css"
+  },
+  {
+    "revision": "2ace96f11a118a1963a4",
+    "url": "/movie_app_2020/static/js/2.23fdfa78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.23fdfa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb26dc17e0d3df93c063",
+    "url": "/movie_app_2020/static/js/main.42814511.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
